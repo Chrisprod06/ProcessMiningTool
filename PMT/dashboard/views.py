f3307a9b15246a13cefdd33e1eecbb30b36714bd
Christophoros Prodromou <christophoros.prodromou@gmail.com>
2022-01-08 00:30:21 +0200

    organization of accounts app

--- PMT/dashboard/views.py
+++ PMT/dashboard/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'dashboard/index.html')
-
-def register(request):
-    return render(request, 'dashboard/register.html')
-
-def login (request):
-    return render(request, 'dashboard/login.html')
